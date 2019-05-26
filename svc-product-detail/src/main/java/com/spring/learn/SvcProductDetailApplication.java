6f54dc18c626cd2ad5459b261ff9eb0be7ad9783
ashish8728 <ashish.mca87@gmail.com>
2019-05-26 10:09:11 +0530

    Added configurations for registration with eureka server.

--- svc-product-detail/src/main/java/com/spring/learn/SvcProductDetailApplication.java
+++ svc-product-detail/src/main/java/com/spring/learn/SvcProductDetailApplication.java
@@ -2,8 +2,10 @@ package com.spring.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SvcProductDetailApplication {
 
 	public static void main(String[] args) {
